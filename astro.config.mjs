d14eb1e6b0561b7c211e629bf4ee7c69660ae6b6
withvoidwithin <92968799+withvoidwithin@users.noreply.github.com>
2026-01-16 10:17:29 +0300

    New Base
    - Reworked full project.

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [starlight({
+		title: 'D2_MODDING',
+		social: [{ icon: 'github', label: 'GitHub', href: 'https://withvoidwithin.github.io/dota2_modding/' }],
+		sidebar: [
+			{
+				label: 'Guides',
+				items: [
+					// Each item here is one entry in the navigation menu.
+					{ label: 'Example Guide', slug: 'guides/example' },
+				],
+			},
+			{
+				label: 'Reference',
+				autogenerate: { directory: 'reference' },
+			},
+		],
+	}), react()],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});

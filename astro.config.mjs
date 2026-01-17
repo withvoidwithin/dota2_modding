b1cf9dafb69ff5658b22d3414d83353074a0476e
withvoidwithin <92968799+withvoidwithin@users.noreply.github.com>
2026-01-17 10:08:40 +0300

    New main page
    - Reworked main page info.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,24 +8,30 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://withvoidwithin.github.io',
-	base: 'dota2_modding',
+	base: '/dota2_modding',
 
 	integrations: [starlight({
 		title: 'D2_MODDING',
 		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withvoidwithin/dota2_modding' }],
+
 		sidebar: [
 			{
 				label: 'Guides',
-				items: [
-					// Each item here is one entry in the navigation menu.
-					{ label: 'Example Guide', slug: 'guides/example' },
-				],
+				autogenerate: { directory: 'guides' },
 			},
 			{
 				label: 'Reference',
 				autogenerate: { directory: 'reference' },
 			},
 		],
+
+		defaultLocale: 'ru',
+		locales: {
+			ru: { label: 'Русский', lang: 'ru' }, // Документация на русском в `src/content/docs/ru/`
+			en: { label: 'English', lang: 'en' }, // Документация на английском в `src/content/docs/en/`
+			'zh-cn': { label: '简体中文', lang: 'zh-CN' }, // Документация на китайском в `src/content/docs/zh-cn/`
+		},
+
 	}), react()],
 
 	vite: {

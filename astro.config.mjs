4534a42b42841bcb6ae2ff5b84235765677ea916
withvoidwithin <92968799+withvoidwithin@users.noreply.github.com>
2026-01-17 19:27:29 +0300

    CustomNetTables
    - Added new page custom_net_tables.
    - Changed main info.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,29 +10,30 @@ export default defineConfig({
 	site: 'https://withvoidwithin.github.io',
 	base: 'dota2_modding',
 
-	integrations: [starlight({
-		title: 'D2_MODDING',
-		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withvoidwithin/dota2_modding' }],
+	integrations: [
+		starlight({
+			title: 'D2_MODDING',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withvoidwithin/dota2_modding' }],
 
-		sidebar: [
-			{
-				label: 'Guides',
-				autogenerate: { directory: 'guides' },
+			sidebar: [
+				{
+					label: 'Documentation',
+					autogenerate: { directory: 'documentation' },
+				},
+			],
+
+			defaultLocale: 'root',
+			locales: {
+				root: { label: 'Русский', lang: 'ru' },
+				en: { label: 'English', lang: 'en' },
+				'zh-cn': { label: '简体中文', lang: 'zh-CN' },
 			},
-			{
-				label: 'Reference',
-				autogenerate: { directory: 'reference' },
+
+			components: {
+				PageTitle: './src/components/overrides/PageTitle.astro', // Подменяем PageTitle на наш кастомный
 			},
-		],
 
-		defaultLocale: 'root',
-		locales: {
-			root: { label: 'Русский', lang: 'ru' },
-			en: { label: 'English', lang: 'en' },
-			'zh-cn': { label: '简体中文', lang: 'zh-CN' },
-		},
-
-	}), react()],
+		}), react()],
 
 	vite: {
 		plugins: [tailwindcss()],
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,20 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({
+		loader: docsLoader(),
+
+		schema: docsSchema({
+			extend: z.object({
+				// Добавляем наш объект meta как опциональный
+				meta: z.object({
+					author: z.string().optional(),
+					game_version: z.string().optional(),
+					date: z.date().or(z.string()).optional(),
+				}).optional(),
+			}),
+		}),
+	}),
 };

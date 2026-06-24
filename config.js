b028a83998fc7d73bc9b71bc7917dc98f2352c8c
withvoidwithin <92968799+withvoidwithin@users.noreply.github.com>
2026-06-24 14:16:48 +0300

    cleanup

--- config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const ROOT = __dirname;
-
-module.exports = {
-    DATA_DIR:      path.join(ROOT, "docs", "data"),
-    MANIFEST_PATH: path.join(ROOT, "docs", "manifest.json"),
-};
